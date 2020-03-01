c7a7a08f7322ec607c450766f591e4aff22f41ec
Alexei <al.turovskiy@gmail.com>
2020-03-01 17:14:25 +0300

    Add listbox and starting add result in text entry.

--- motocross_api.py
+++ motocross_api.py
@@ -4,33 +4,47 @@ from tkinter import ttk
 import json
 
 def get_info(*args):
-    result_text.delete("1.0", "end")
+    riders_listbox.delete("0", "end")
     try:
         year = year_list.get()
         track_name = track_list.get()
         class_moto = class_list.get()
-        #moto_number
-        url = f"https://api.promotocrossapi.com/laptimes/{year}/{track_name}/{class_moto}"
+        moto_number = int(moto_list.get())
+        url = f"https://api.promotocrossapi.com/laptimes/{year}/{track_name}/{class_moto}/{moto_number}"
         request = requests.get(url)
         result = request.json()
-        for item in result["data"]:
+        for rider in result["data"]["riderData"]:
             try:
-                result_text.insert("end", item["race"])
-                result_text.insert("end", "\n")
+                riders_listbox.insert("end", rider["name"])
+                riders_result.append(rider["name"])
+                riders_info.append(rider["name"])
+                for key, value in rider.items():
+                    if key == "bike" or  key == "number":
+                        riders_info.append(value)
+                    elif key == "name":
+                        pass
+                    else:
+                        riders_result.append(value)
             except KeyError:
                 pass
-            for rider in item["riderData"]:
-                try:
-                    result_text.insert("end", rider["name"])
-                    result_text.insert("end", "\n")
-                except KeyError:
-                    pass
+    except ValueError:
+        pass
+
+def get_time(*args):
+    result_text.delete("1.0", "end")
+    try:
+        for item in riders_result:
+            result_text.insert("end", item)
+            result_text.insert("end", "\n")
     except ValueError:
         pass
 
 
 root = Tk()
 root.title("Motocross result")
+
+riders_result = []
+riders_info = []
 
 main_window = ttk.Frame(root)
 main_window.grid(column = 0, row = 0)
@@ -47,10 +61,27 @@ class_list = ttk.Combobox(main_window)
 class_list["values"] = (250, 450)
 class_list.grid(column = 0, row = 2)
 
-ttk.Label(main_window, text="Motocross result").grid(column=0, row=3)
-result_text = Text(main_window, width=50, height=10)
-result_text.grid(column = 0, row = 4, columnspan=2)
+moto_list = ttk.Combobox(main_window)
+moto_list["values"] = (1, 2)
+moto_list.grid(column = 0, row = 3)
+
+ttk.Label(main_window, text="Riders").grid(column=0, row=4)
+riders_listbox = Listbox(main_window, height=10)
+riders_listbox.grid(column = 0, row = 5)
+
+scroll_bar_listbox = ttk.Scrollbar(main_window, orient=VERTICAL, command=riders_listbox.yview)
+scroll_bar_listbox.grid(column=0, row=5, sticky=(N,E,S))
+riders_listbox.configure(yscrollcommand=scroll_bar_listbox.set)
+
+result_text = Text(main_window, width=30, height=10)
+result_text.grid(column = 1, row = 5)
+
+scroll_bar_textbox = ttk.Scrollbar(main_window, orient=VERTICAL, command=result_text.yview)
+scroll_bar_textbox.grid(column=1, row=5, sticky=(N,E,S))
+result_text.configure(yscrollcommand=scroll_bar_textbox.set)
 
 ttk.Button(main_window, text="Get result", command=get_info).grid(column=1, row=0, rowspan=3)
+
+riders_listbox.bind('<<ListboxSelect>>', get_time)
 
 root.mainloop()
